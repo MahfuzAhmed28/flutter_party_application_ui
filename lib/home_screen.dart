a5c9c7172796dcf4a9bcac7ca6d8d7550d35f335
MahfuzAhmed28 <mahfuzyasin28@gmail.com>
2026-01-12 22:42:05 +0600

    Second Commit

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -32,7 +32,9 @@ class _HomeScreenState extends State<HomeScreen> {
           ),
           child: Column(
             children: [
-              Text('Find the best locations near to you for a good night')
+              Text('Find the best locations near to you for a good night',style: TextStyle(
+
+              ),)
             ],
           ),
         ),
